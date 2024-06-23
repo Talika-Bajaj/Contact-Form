dd71d396546c7329ab619e21ee805cf2a6be3f65
Talika Bajaj <bajajtalika2001@gmail.com>
2024-06-23 23:38:04 +0530

    Add files

---
+++ README.md
@@ -0,0 +1,11 @@
+# Frontend Mentor - Contact form
+
+![Design preview for the Contact form coding challenge](./design/desktop-preview.jpg)
+
+## Hi! 👋
+
+This is my 28th Frontend Mentor Coding Challenge.
+
+I completed a Frontend Mentor challenge by creating an accessible contact form using HTML, CSS, and JavaScript. This project emphasized the importance of building forms with various input types and implementing validation. It was an excellent practice for enhancing my skills in creating user-friendly and accessible web forms.
+
+**Had fun building!** 🚀
---
+++ index.js
@@ -0,0 +1,123 @@
+const queryRadio = document.getElementsByClassName('query-radio');
+const queryGroup = document.getElementsByClassName('query-group');
+const inputs = document.getElementsByClassName('inputs');
+const form = document.getElementById('input-form');
+const errors = document.getElementsByClassName('error');
+const valid = document.querySelector('.valid');
+const consent = document.getElementById('consent');
+const consentError = document.querySelector('.consent-error');
+const msgArea = document.getElementById('msg-area');
+const msgError = document.querySelector('.msg-error');
+const pop = document.querySelector('.pop-up');
+
+
+form.addEventListener('submit', (event)=> {
+    event.preventDefault();
+    Array.from(inputs).forEach((input) => {
+        if ((input.value == '' || msgArea.value === '' || consent.checked == false ))  {
+            pop.style.display = 'none';
+        } else {
+            pop.style.display = 'flex';
+
+        }
+    }) 
+
+    checkFields();
+    checkRadios();
+    validateEmail();    
+    checkTextarea();    
+    checkBox();     
+});
+
+// form.addEventListener('click', checkRadios);
+// for input fields
+function checkFields() {
+    Array.from(inputs).forEach((input, index) => {
+        if (input.value == '' ) {
+            errors[index].style.display = 'block';
+            input.style.border = '1px solid hsl(0, 66%, 54%)';
+        } else {
+            input.style.border = '1px solid hsl(186, 15%, 59%)';
+            errors[index].style.display = 'none';
+        }
+    }) 
+
+}
+
+//for email
+function validateEmail () {
+    const format = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    let email = document.getElementById('email');
+    if (email.value !== '') {
+
+        if (email.value.match(format)) {
+            document.querySelector('.valid').style.display = 'none';
+            email.style.border = '1px solid hsl(186, 15%, 59%)';
+        } else {
+            document.querySelector('.valid').style.display = 'block';
+            email.style.border = '1px solid hsl(0, 66%, 54%)'
+        }
+    }
+}
+
+function checkRadios() {
+    const queryMsg = document.getElementById('query-msg');
+    const radioName = document.getElementsByName('query');
+    let isChecked = false;
+
+     for (let i = 0; i < radioName.length; i++) {
+        if (radioName[i].checked) {
+            isChecked = true;
+            break;
+        }
+    }
+    
+    if (!isChecked) {
+        queryMsg.classList.remove('hidden');
+        console.log('checked');
+    } else {
+      queryMsg.classList.add('hidden')
+      console.log('unchecked');
+   }
+}
+
+//for textarea
+function checkTextarea() {
+    if (msgArea.value === '' ) {
+        msgError.style.display = 'block';
+        msgArea.style.border = '1px solid hsl(0, 66%, 54%)'
+    } else {
+        msgError.style.display = 'none';
+        msgArea.style.border = '1px solid hsl(186, 15%, 59%)';
+    }
+}
+    
+//for checkbox
+function checkBox() {
+    if ( consent.checked == false ){
+        consentError.style.display = 'block';
+    } else {
+        consentError.style.display = 'none';
+    }
+}
+
+
+function changeColor () {
+    const radioButtons = document.querySelectorAll('input[name="query"]');
+    
+    radioButtons.forEach(function(radio) {
+        radio.addEventListener('change', function() {
+            const queryGroups = document.querySelectorAll('.query-group');
+            
+            queryGroups.forEach(function(group) {
+                group.classList.remove('selected');
+            });
+            
+            if (this.checked) {
+                this.parentElement.classList.add('selected');
+            }
+        });
+    });
+}
+
+changeColor();
